af27f6518315102ee82c009e00f53e980e7bde12
lkolebka <94646506+lkolebka@users.noreply.github.com>
2023-01-28 14:50:32 +0100

    small fix syntax

--- insertTeams.py
+++ insertTeams.py
@@ -64,70 +64,76 @@ for row in ws.rows:
 
 
 
-    # Check if the player3 name is not empty
-    if player3_name:
-      # Check if the player already exists in the Players table
-      cur.execute("SELECT id FROM players WHERE name=%s", (player3_name,))
-      player3_id = cur.fetchone()
-      if player3_id is None:
-        # If the player does not exist, insert them into the players table with a unique id
-        cur.execute("SELECT nextval('players_id_seq')")
-        id = cur.fetchone()[0]
-        cur.execute("INSERT INTO players (id, name) VALUES (%s, %s)", (id, player3_name))
-        player3_id = id
-      else:
-        # If the player already exists, retrieve their id
-        player3_id = player3_id[0]
+  # Check if the player3 name is not empty
+  if player3_name:
+    # Check if the player already exists in the Players table
+    cur.execute("SELECT id FROM players WHERE name=%s", (player3_name,))
+    player3_id = cur.fetchone()
+    if player3_id is None:
+      # If the player does not exist, insert them into the players table with a unique id
+      cur.execute("SELECT nextval('players_id_seq')")
+      id = cur.fetchone()[0]
+      cur.execute("INSERT INTO players (id, name) VALUES (%s, %s)", (id, player3_name))
+      player3_id = id
+    else:
+      # If the player already exists, retrieve their id
+      player3_id = player3_id[0]
 
 
 
-    # Check if the player4 name is not empty
-    if player4_name:
-      # Check if the player already exists in the Players table
-      cur.execute("SELECT id FROM players WHERE name=%s", (player4_name,))
-      player4_id = cur.fetchone()
-      if player4_id is None:
-        # If the player does not exist, insert them into the players table with a unique id
-        cur.execute("SELECT nextval('players_id_seq')")
-        id = cur.fetchone()[0]
-        cur.execute("INSERT INTO players (id, name) VALUES (%s, %s)", (id, player4_name))
-        player4_id = id
-      else:
-        # If the player already exists, retrieve their id
-        player4_id = player4_id[0]
+  # Check if the player4 name is not empty
+  if player4_name:
+    # Check if the player already exists in the Players table
+    cur.execute("SELECT id FROM players WHERE name=%s", (player4_name,))
+    player4_id = cur.fetchone()
+    if player4_id is None:
+      # If the player does not exist, insert them into the players table with a unique id
+      cur.execute("SELECT nextval('players_id_seq')")
+      id = cur.fetchone()[0]
+      cur.execute("INSERT INTO players (id, name) VALUES (%s, %s)", (id, player4_name))
+      player4_id = id
+    else:
+      # If the player already exists, retrieve their id
+      player4_id = player4_id[0]
 
 
   
 
 
-    def get_team_ids(player1_id, player2_id, player3_id, player4_id, cur):
-        # Check if the team already exists in the Teams table
-      cur.execute("SELECT id FROM teams WHERE (team_player_1_id=%s AND team_player_2_id=%s) OR (team_player_1_id=%s AND team_player_2_id=%s)", (player1_id, player2_id, player2_id, player1_id))
-      team_player_1_id = cur.fetchone()
-      if team_player_1_id is None:
-        # If the team does not exist, insert them into the teams table with a unique id
-        cur.execute("SELECT nextval('teams_id_seq')")
-        id = cur.fetchone()[0]
-        cur.execute("INSERT INTO teams (id, team_player_1_id, team_player_2_id) VALUES (%s, %s, %s)", (id, player1_id, player2_id))
-        team_player_1_id = id
-      else:
-        # If the team already exists, retrieve their id
-        team_player_1_id = team_player_1_id[0]
+  def get_team_ids(player1_id, player2_id, player3_id, player4_id, cur):
+      # Check if the team already exists in the Teams table
+    cur.execute("SELECT id FROM teams WHERE (team_player_1_id=%s AND team_player_2_id=%s) OR (team_player_1_id=%s AND team_player_2_id=%s)", (player1_id, player2_id, player2_id, player1_id))
+    team_player_1_id = cur.fetchone()
+    if team_player_1_id is None:
+      # If the team does not exist, insert them into the teams table with a unique id
+      cur.execute("SELECT nextval('teams_id_seq')")
+      id = cur.fetchone()[0]
+      cur.execute("INSERT INTO teams (id, team_player_1_id, team_player_2_id) VALUES (%s, %s, %s)", (id, player1_id, player2_id))
+      team_player_1_id = id
+    else:
+      # If the team already exists, retrieve their id
+      team_player_1_id = team_player_1_id[0]
+    
+    # Repeat the process for the other team
+    cur.execute("SELECT id FROM teams WHERE (team_player_1_id=%s AND team_player_2_id=%s) OR (team_player_1_id=%s AND team_player_2_id=%s)", (player3_id, player4_id, player4_id, player3_id))
+    team_player_2_id = cur.fetchone()
+    if team_player_2_id is None:
+      cur.execute("SELECT nextval('teams_id_seq')")
+      id = cur.fetchone()[0]
+      cur.execute("INSERT INTO teams (id, team_player_1_id, team_player2) VALUES (%s, %s, %s)", (id, player3_id, player4_id))
+      team_player_2_id = id
+    else:
+      team_player_2_id = team_player_2_id[0]
+    
+    return team_player_1_id, team_player_2_id
+    
+
+  
       
-      # Repeat the process for the other team
-      cur.execute("SELECT id FROM teams WHERE (team_player_1_id=%s AND team_player_2_id=%s) OR (team_player_1_id=%s AND team_player_2_id=%s)", (player3_id, player4_id, player4_id, player3_id))
-      team_player_2_id = cur.fetchone()
-      if team_player_2_id is None:
-        cur.execute("SELECT nextval('teams_id_seq')")
-        id = cur.fetchone()[0]
-        cur.execute("INSERT INTO teams (id, team_player_1_id, team_player2) VALUES (%s, %s, %s)", (id, player3_id, player4_id))
-        team_player_2_id = id
-      else:
-        team_player_2_id = team_player_2_id[0]
-        
-      return team_player_1_id, team_player_2_id
+
 
   # Commit the changes to the database
+
   conn.commit()
 
 
