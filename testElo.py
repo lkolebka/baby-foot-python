ca2b58b1867bfb382197af3b7c4919e252a2c091
lkolebka <94646506+lkolebka@users.noreply.github.com>
2023-02-23 20:59:42 +0100

    fixing bugs

--- testElo.py
+++ testElo.py
@@ -1,9 +1,14 @@
+import math
 a = 1500 
 b = 1600
+
 teamAB = a + b
 print('teamAB =', teamAB)
 
 newrating = 1 + (a - teamAB)/1000
 print('newrating =', newrating)
 
+score_difference = 11-6
 
+calculate_point_factor =  1 + (math.log(score_difference + 1) / math.log(25))
+print("calculate_point_factor = ", calculate_point_factor)
