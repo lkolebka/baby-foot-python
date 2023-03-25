316828ae0710552ab4575d3d80e6380edaef2900
lkolebka <94646506+lkolebka@users.noreply.github.com>
2023-03-25 17:53:33 +0100

    fixing date

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 import psycopg2
 from config import DATABASE_CONFIG
-import datetime
+from datetime import datetime
 import math
 
 
