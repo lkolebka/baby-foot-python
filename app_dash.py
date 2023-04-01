cad04a18bf805ff497e5c11a6a5756d0e0cf9d48
lkolebka <94646506+lkolebka@users.noreply.github.com>
2023-04-01 16:04:25 +0200

    chart on dash

--- app_dash.py
+++ app_dash.py
@@ -9,6 +9,9 @@ from sqlalchemy import create_engine
 from config import DATABASE_CONFIG
 from app import get_players
 
+fontFormat = dict(family="sans-serif",
+                      size=22,)
+
 # create database connection
 engine = create_engine(
     f"postgresql://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@"
@@ -21,21 +24,20 @@ app = dash.Dash(__name__, external_stylesheets=['/static/style.css'])
 server = app.server
 
 app.layout = html.Div([
-    html.H1('Player Rating Evolution Dashboard'),
+    html.H1('Rating Evolution'),
     html.Link(href='/static/style.css', rel='stylesheet'),
     html.Div(className='dropdown-container', children=[
-
        dcc.Dropdown(
             id='player-dropdown',
             options=[{'label': player, 'value': player} for player in get_players()],
             value=['Matthieu', 'Lazare'],
             multi=True
         )
-    ], style={'width': '50%', 'margin': 'auto'}), # set the width and center the container horizontally
+    ], style={'width': '50%', 'margin': 'auto'}),
     html.Div(className='chart-container', children=[
-        dcc.Graph(id='rating-graph')
-    ], style={'width': '80%', 'margin': 'auto'}), # set the width and center the container horizontally
-], style={'max-width': '1200px', 'margin': 'auto'}) # set the maximum width of the layout and center it horizontally
+        dcc.Graph(id='rating-graph', style={'width': '100%', 'height': '100%'})
+    ], style={'width': '100%', 'height': 'calc(100vh - 200px)'})
+], style={'max-width': '1200px', 'margin': 'auto'})
 
 
 @app.callback(
@@ -57,10 +59,33 @@ def update_rating_graph(players):
                     ORDER BY week_start ASC"""
         data = pd.read_sql(query, engine)
         fig.add_trace(go.Scatter(x=data['week_start'], y=data['rating'], name=player))
-    fig.update_layout(title='Player Rating Evolution',
-                      xaxis_title='Week',
-                      yaxis_title='Rating')
+    fig.update_xaxes(title_text='')
+    fig.update_yaxes(title_text='')
+    fig.update_layout(yaxis={'categoryorder':'total ascending'})
+    fig.update_layout(font=fontFormat)
+    fig.update_yaxes(ticksuffix = "  ")
+    fig.update_layout(legend_orientation="h")
+    
+    # Set different width and height values based on screen size
+    fig.update_layout(
+        width=350,
+        height=500,
+        margin=dict(l=30, r=30, t=50, b=30),
+        paper_bgcolor="white",
+        plot_bgcolor="white",
+    )
+    fig.update_layout(
+        {
+            "title": {
+                "font": {"size": 12},
+            }
+        }
+    )
+    
     return fig
 
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.static_folder = 'static'
+    app.run(host='0.0.0.0', port=8082) 
