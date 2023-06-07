c6c82fcd73489ae36a867d626037e651469a0839
lkolebka <94646506+lkolebka@users.noreply.github.com>
2023-06-07 20:25:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ if ENV == 'dev':
         'host': "localhost",
         'database': "babyfoot",
         'user': "postgres",
-        'password': "519173"
+        'password': "**"**
     }
 else: 
 
-    uri = "postgres://mzallduhesqqds:f18bf72dfd0e2415db010290b8192be8c92e4e1d257205808e9dedead1d0c091@ec2-44-214-132-149.compute-1.amazonaws.com:5432/dd2oto7f7bkek8"
+    uri = "****"
     parsed_uri = urlparse(uri)
 
     DATABASE_CONFIG = {
-        'host': "ec2-44-214-132-149.compute-1.amazonaws.com",
-        'database': "dd2oto7f7bkek8",
-        'user': "mzallduhesqqds",
-        'password': "f18bf72dfd0e2415db010290b8192be8c92e4e1d257205808e9dedead1d0c091",
-        'port': "5432"
+        'host': "****",
+        'database': "****",
+        'user': "****",
+        'password': "****",
+        'port': "****"
     } 
